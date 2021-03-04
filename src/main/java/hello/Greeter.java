76a03d23e9cbaf60bdfbed68a4a1a8bcfdcbf519
Suvin Ganjoo <suvin.ganjoo@grabtaxi.com>
2021-03-04 13:54:06 +0800

    changes in Greeter file

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Project for AchiStar Technologies";
+        return "DevOps Project for AchiStar Technologies";
     }
 }
