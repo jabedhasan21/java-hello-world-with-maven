9d971b27541c2a7f0b68b1c2295458a55f8de3bd
muhamadjibril <ibiljibril@student.telkomuniversity.ac.id>
2022-06-29 13:06:41 +0700

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -8,6 +8,6 @@ public class HelloWorld {
 		  System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        System.out.println(greeter.sayHi());
     }
 }
